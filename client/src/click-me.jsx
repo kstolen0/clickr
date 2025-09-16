ae49f639a3261dd464cc97b8a449a00070ad198b
Kristian Stolen <kstolen0@gmail.com>
2025-09-16 21:38:31 +0800

    updates

--- client/src/click-me.jsx
+++ client/src/click-me.jsx
@@ -6,24 +6,29 @@ export const ClickMe = () => {
   const [count, setCount] = useState(0)
 
   useEffect(() => {
-    if (count > total) {
-      setTotal(total + 10)
-      setCount(0)
+    if (count == total) {
+      setTimeout(() => {
+        setTotal(total + 10)
+        setCount(0)
+      }, 500)
     }
   }, [count])
 
   const barStyle = {
-    width: `${(count / total) * 100}%`
+    width: `${(count / total) * 100}%`,
+    visibility: count > 0 ? 'visible' : 'hidden',
   }
 
-  const increment = () => setCount(count + 1)
+  const increment = () => {
+    if (count < total) setCount(count + 1)
+  }
 
   return (
     <div className="click-me">
       <div className="content">
         <span className="current-count">{count}</span>
         <div className="progress">
-          <div className="progress-bar" style={barStyle}></div>
+          <div className="progress-bar" style={barStyle}>{count}</div>
         </div>
         <button onClick={increment}>click me</button>
       </div>
