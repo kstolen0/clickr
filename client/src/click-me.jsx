169f2859550526a87f2b3e014010622e7c93667c
Kristian Stolen <kstolen0@gmail.com>
2025-09-16 21:26:34 +0800

    progress bar

--- client/src/click-me.jsx
+++ client/src/click-me.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export const ClickMe = () => {
 
-  const total = 10
+  const [total, setTotal] = useState(10)
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    if (count > total) {
+      setTotal(total + 10)
+      setCount(0)
+    }
+  }, [count])
+
+  const barStyle = {
+    width: `${(count / total) * 100}%`
+  }
+
+  const increment = () => setCount(count + 1)
 
   return (
     <div className="click-me">
-      <span className="current-count">{count}</span>
-      <div className="progress">{total}</div>
-      <button onClick={() => setCount(count + 1)}>click me</button>
+      <div className="content">
+        <span className="current-count">{count}</span>
+        <div className="progress">
+          <div className="progress-bar" style={barStyle}></div>
+        </div>
+        <button onClick={increment}>click me</button>
+      </div>
     </div>
   )
 }
