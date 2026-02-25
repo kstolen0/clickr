abe8935fa157c5e8561d770a40b2a8b6b27d53cd
Kristian Stolen <kstolen0@gmail.com>
2026-02-25 16:15:13 +0800

    make counter decrement

--- src/main/kotlin/Sockets.kt
+++ src/main/kotlin/Sockets.kt
@@ -4,10 +4,15 @@ import io.ktor.serialization.kotlinx.KotlinxWebsocketSerializationConverter
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.util.Collections
+import kotlin.concurrent.atomics.AtomicInt
 import kotlin.concurrent.atomics.ExperimentalAtomicApi
+import kotlin.concurrent.atomics.decrementAndFetch
+import kotlin.concurrent.atomics.incrementAndFetch
 import kotlin.time.Duration.Companion.seconds
 
 @OptIn(ExperimentalAtomicApi::class)
@@ -20,32 +25,44 @@ fun Application.configureSockets() {
         masking = false
     }
     routing {
-        var count = 0
-        var target = 10
-        var style = 0
-
+        var count = AtomicInt(0)
+        var target = AtomicInt(10)
         val sessions = Collections.synchronizedList<WebSocketServerSession>(ArrayList())
+        var style = AtomicInt(0)
+
+       launch {
+          while (true) {
+             delay (2000)
+              val countVal = count.load()
+              if (countVal > 0) {
+                  count.decrementAndFetch()
+                  updateEveryone(sessions, count, target, style)
+              }
+          }
+       }
 
         webSocket("/click") {
             sessions.add(this)
             try {
-                sendSerialized(Response(count, target, style))
+                sendSerialized(Response(count.load(), target.load(), style.load()))
 
                 while (true) {
                     val cmd = receiveDeserialized<Command>()
                     if (cmd.command == "inc") {
-                        count++
+                        val countVal = count.incrementAndFetch()
+                        var targetVal = target.load()
+                        if (countVal == targetVal) {
+                            targetVal*=2
+                            count.store(0)
+                            target.store(targetVal)
+                            val styleVal = style.load() + 1
+                            style.store(styleVal % 5)
+                        }
+
+                       updateEveryone(sessions, count, target, style)
+
                     }
 
-                    if (count == target) {
-                        target*=2
-                        count = 0
-                        style = if (style >= 4) 0 else style + 1
-                    }
-
-                    for (session in sessions) {
-                        session.sendSerialized(Response(count, target, style))
-                    }
 
                 }
             }
@@ -56,6 +73,14 @@ fun Application.configureSockets() {
             }
         }
     }
+}
+
+@OptIn(ExperimentalAtomicApi::class)
+suspend fun updateEveryone(sessions: Collection<WebSocketServerSession>, count: AtomicInt, target: AtomicInt, style: AtomicInt) {
+    for (session in sessions) {
+        session.sendSerialized(Response(count.load(), target.load(), style.load()))
+    }
+
 }
 
 @Serializable
